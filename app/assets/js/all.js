1fdaa4a40e2066d16f69abb6ab3489834625c296
a3803896 <a3803896@gmail.com>
2020-05-22 18:05:00 +0800

    1090522 layout index完工

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -13,4 +13,13 @@ $(document).ready(() => {
     e.preventDefault();
     $("#rotate").toggleClass("rotate");
   });
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    centeredSlides: false,
+    spaceBetween: null,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 });
