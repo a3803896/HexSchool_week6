fded86c5c52725dac8ef2f3ec36f81df44c32fe4
a3803896 <a3803896@gmail.com>
2020-05-23 14:49:06 +0800

    1090523 shop完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -13,6 +13,11 @@ $(document).ready(() => {
     e.preventDefault();
     $("#rotate").toggleClass("rotate");
   });
+
+  $("#button-addon2").click(function (e) {
+    e.preventDefault();
+    alert("Successfully subscribed")
+  });
   var swiper = new Swiper('.swiper-container', {
     slidesPerView: 'auto',
     centeredSlides: false,
@@ -22,4 +27,17 @@ $(document).ready(() => {
       clickable: true,
     },
   });
+
+  // 換頁時切換layout的css樣式
+  $('.hamburgerIcon,.cart,.header h1 a,.header h3 a').each(function () {
+    if (window.location.href === "http://localhost:8080/shop.html") {
+      $(this).addClass('maincolor');
+    }
+  });
+  $('.linkInner>h3:first-child>a').each(function () {
+    if (window.location.href === "http://localhost:8080/shop.html") {
+      $(this).addClass('nowPage');
+    }
+  });
+
 });
